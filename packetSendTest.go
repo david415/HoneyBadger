c136e5cd58dddfdd5b97b7c4b65e338f41d0b09d
David Stainton <dstainton415@gmail.com>
2014-12-01 03:59:08 +0000

    Add sanitizeTCPFields to check if the tcp layer is malformed
    here we only actually check that the src and dst ports are set correctly...

--- packetSendTest.go
+++ packetSendTest.go
@@ -4,9 +4,28 @@ import (
 	"code.google.com/p/gopacket"
 	"code.google.com/p/gopacket/examples/util"
 	"code.google.com/p/gopacket/layers"
+	"fmt"
 	"log"
 	"net"
 )
+
+func sanitizeTCPFields(packetData []byte, srcPort, dstPort layers.TCPPort) error {
+	packet := gopacket.NewPacket(packetData, layers.LayerTypeTCP, gopacket.Default)
+	tcpLayerType := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayerType == nil {
+		return fmt.Errorf("packet has no tcp layer\n")
+	}
+	tcpLayer, ok := tcpLayerType.(*layers.TCP)
+	if !ok {
+		return fmt.Errorf("tcp layer is not tcp layer :-/")
+	}
+
+	if srcPort != tcpLayer.SrcPort || dstPort != tcpLayer.DstPort {
+		return fmt.Errorf("malformed tcp layer: srcport %d dstport %d\n", tcpLayer.SrcPort, tcpLayer.DstPort)
+	}
+
+	return nil
+}
 
 func main() {
 	defer util.Run()()
@@ -48,32 +67,31 @@ func main() {
 	tcp := layers.TCP{
 		SrcPort: srcport,
 		DstPort: dstport,
-
-		Seq:    1105024978,
-		Ack:    0,
-		ACK:    false,
-		SYN:    true,
-		FIN:    false,
-		RST:    false,
-		URG:    false,
-		ECE:    false,
-		CWR:    false,
-		NS:     false,
-		PSH:    false,
-		Window: 14600,
+		Urgent:  0,
+		Seq:     11050,
+		Ack:     0,
+		ACK:     false,
+		SYN:     false,
+		FIN:     false,
+		RST:     false,
+		URG:     false,
+		ECE:     false,
+		CWR:     false,
+		NS:      false,
+		PSH:     false,
 	}
 
-	payload := gopacket.Payload([]byte("meowmeowmeowXXXhoho"))
+	//payload := gopacket.Payload([]byte("meowmeowmeowXXXhoho"))
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{
-		//		FixLengths:       true,
+		FixLengths:       true,
 		ComputeChecksums: true,
 	}
 	tcp.SetNetworkLayerForChecksum(&ip)
 	err := gopacket.SerializeLayers(buf, opts,
 		&ip,
-		&tcp,
-		payload)
+		&tcp)
+	//payload)
 	if err != nil {
 		panic(err)
 	}
@@ -84,6 +102,12 @@ func main() {
 	ipConn, err := net.ListenPacket("ip4:tcp", "0.0.0.0")
 	if err != nil {
 		panic(err)
+	}
+
+	err = sanitizeTCPFields(packetData, srcport, dstport)
+	if err != nil {
+		//panic(err)
+		log.Printf("malformed packet: %s\n", err)
 	}
 
 	dstIPaddr := net.IPAddr{
