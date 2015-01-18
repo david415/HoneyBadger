785cbfbeaab8f2f5bbb9f9bd52d136b7dd21f58d
David Stainton <dstainton415@gmail.com>
2015-01-18 03:08:27 +0000

    Add TestGetOverlapRingsWithZeroRings
    Another unit test... but I need to add more tests for
    this unit of code because it has bugs and is broken.

--- tcpFlowTracking_test.go
+++ tcpFlowTracking_test.go
@@ -7,6 +7,37 @@ import (
 	"testing"
 )
 
+func TestGetOverlapRingsWithZeroRings(t *testing.T) {
+	ip := layers.IPv4{
+		SrcIP:    net.IP{1, 2, 3, 4},
+		DstIP:    net.IP{2, 3, 4, 5},
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolTCP,
+	}
+	tcp := layers.TCP{
+		SYN:     true,
+		SrcPort: 1,
+		DstPort: 2,
+	}
+	tcp.SetNetworkLayerForChecksum(&ip)
+	payload := gopacket.Payload([]byte{1, 2, 3, 4})
+	flow := NewTcpIpFlowFromLayers(ip, tcp)
+	p := PacketManifest{
+		IP:      ip,
+		TCP:     tcp,
+		Payload: payload,
+	}
+	conn := NewConnection()
+	head, tail := conn.getOverlapRings(p, flow)
+	if head == nil || tail == nil {
+		return
+	} else {
+		t.Fail()
+	}
+	return
+}
+
 func TestSequenceFromPacket(t *testing.T) {
 	var testSeq uint32 = 12345
 	buf := gopacket.NewSerializeBuffer()
