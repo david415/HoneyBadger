f2a4b9649edfec7b33148a30afa2639af901e01a
David Stainton <dstainton415@gmail.com>
2015-01-28 06:37:38 +0000

    Fix TestSequenceFromPacket and move to flow_test.go

--- flow_test.go
+++ flow_test.go
@@ -8,6 +8,46 @@ import (
 	"testing"
 )
 
+func TestSequenceFromPacket(t *testing.T) {
+	var testSeq uint32 = 12345
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	ip := layers.IPv4{
+		SrcIP:    net.IP{1, 2, 3, 4},
+		DstIP:    net.IP{2, 3, 4, 5},
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolTCP,
+	}
+	tcp := layers.TCP{
+		SYN:       true,
+		SrcPort:   1,
+		DstPort:   2,
+		Seq:       testSeq,
+		BaseLayer: layers.BaseLayer{Payload: []byte{1, 2, 3}},
+	}
+	tcp.SetNetworkLayerForChecksum(&ip)
+	gopacket.SerializeLayers(buf, opts, &ip, &tcp)
+	packetData := buf.Bytes()
+	seq, err := SequenceFromPacket(packetData)
+	if err == nil && seq == testSeq {
+		return
+	} else {
+		t.Error("SequenceFromPacket failed")
+		t.Fail()
+		return
+	}
+
+	seq, err = SequenceFromPacket([]byte{1, 2, 3})
+	if err == nil {
+		t.Error("SequenceFromPacket should have failed")
+		t.Fail()
+	}
+}
+
 func TestFlowString(t *testing.T) {
 	ipFlow, _ := gopacket.FlowFromEndpoints(layers.NewIPEndpoint(net.IPv4(1, 2, 3, 4)), layers.NewIPEndpoint(net.IPv4(2, 3, 4, 5)))
 	tcpFlow, _ := gopacket.FlowFromEndpoints(layers.NewTCPPortEndpoint(layers.TCPPort(1)), layers.NewTCPPortEndpoint(layers.TCPPort(2)))
--- inquisition_test.go
+++ inquisition_test.go
@@ -832,39 +832,6 @@ func TestGetOverlapRings(t *testing.T) {
 	return
 }
 
-func TestSequenceFromPacket(t *testing.T) {
-	var testSeq uint32 = 12345
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		FixLengths:       true,
-		ComputeChecksums: true,
-	}
-	ip := layers.IPv4{
-		SrcIP:    net.IP{1, 2, 3, 4},
-		DstIP:    net.IP{2, 3, 4, 5},
-		Version:  4,
-		TTL:      64,
-		Protocol: layers.IPProtocolTCP,
-	}
-	tcp := layers.TCP{
-		SYN:       true,
-		SrcPort:   1,
-		DstPort:   2,
-		Seq:       testSeq,
-		BaseLayer: layers.BaseLayer{Payload: []byte{1, 2, 3}},
-	}
-	tcp.SetNetworkLayerForChecksum(&ip)
-	gopacket.SerializeLayers(buf, opts, &ip, &tcp)
-	packetData := buf.Bytes()
-	seq, err := SequenceFromPacket(packetData)
-	if err == nil && seq == testSeq {
-		return
-	} else {
-		t.Fail()
-		return
-	}
-}
-
 func TestStateDataTransfer(t *testing.T) {
 	conn := NewConnection()
 	conn.AttackLogger = NewDummyAttackLogger()
