235de812e875383f0255aed9b8f509b796fec407
David Stainton <dstainton415@gmail.com>
2015-02-02 21:41:03 +0000

    Remove connection pool delete panic test
    Conflicts:
    	connection_pool_test.go

--- connection_pool_test.go
+++ connection_pool_test.go
@@ -144,28 +144,6 @@ func TestConnectionPool(t *testing.T) {
 
 }
 
-func TestConnectionPoolDeletePanic(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Error("failed to panic")
-			t.Fail()
-		}
-	}()
-
-	connPool := NewConnectionPool()
-	conn := NewConnection(connPool)
-	ipFlow, _ := gopacket.FlowFromEndpoints(layers.NewIPEndpoint(net.IPv4(1, 2, 3, 4)), layers.NewIPEndpoint(net.IPv4(2, 3, 4, 5)))
-	tcpFlow, _ := gopacket.FlowFromEndpoints(layers.NewTCPPortEndpoint(layers.TCPPort(1)), layers.NewTCPPortEndpoint(layers.TCPPort(2)))
-	flow := NewTcpIpFlowFromFlows(ipFlow, tcpFlow)
-	conn.clientFlow = flow
-	connPool.Put(flow, conn)
-	conn.state = TCP_DATA_TRANSFER
-	packetManifest := PacketManifest{}
-	conn.receivePacket(packetManifest, flow, time.Now())
-	connPool.Delete(flow)
-	connPool.Delete(flow)
-}
-
 func TestRemoveFromPool(t *testing.T) {
 	connPool := NewConnectionPool()
 	conn := NewConnection(connPool)
