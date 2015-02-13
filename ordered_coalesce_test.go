1f19af93879e73f887b7b9b519c233725e65441e
David Stainton <dstainton415@gmail.com>
2015-02-13 07:06:27 +0000

    Add unit tests for OrderedCoalesce
    This is only partial unit test coverage for OrderedCoalesce.

---
+++ ordered_coalesce_test.go
@@ -0,0 +1,54 @@
+package HoneyBadger
+
+import (
+	"code.google.com/p/gopacket"
+	"code.google.com/p/gopacket/layers"
+	"container/ring"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestOrderedCoalesce(t *testing.T) {
+	maxBufferedPagesTotal := 1024
+	maxBufferedPagesPerFlow := 1024
+	streamRing := ring.New(MAX_CONN_PACKETS)
+	pager := NewPager()
+
+	ipFlow, _ := gopacket.FlowFromEndpoints(layers.NewIPEndpoint(net.IPv4(1, 2, 3, 4)), layers.NewIPEndpoint(net.IPv4(2, 3, 4, 5)))
+	tcpFlow, _ := gopacket.FlowFromEndpoints(layers.NewTCPPortEndpoint(layers.TCPPort(1)), layers.NewTCPPortEndpoint(layers.TCPPort(2)))
+	flow := NewTcpIpFlowFromFlows(ipFlow, tcpFlow)
+
+	coalesce := NewOrderedCoalesce(flow, pager, streamRing, maxBufferedPagesTotal, maxBufferedPagesPerFlow)
+
+	ip := layers.IPv4{
+		SrcIP:    net.IP{1, 2, 3, 4},
+		DstIP:    net.IP{2, 3, 4, 5},
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolTCP,
+	}
+	tcp := layers.TCP{
+		Seq:     3,
+		SYN:     false,
+		SrcPort: 1,
+		DstPort: 2,
+	}
+	p := PacketManifest{
+		Timestamp: time.Now(),
+		Flow:      flow,
+		IP:        ip,
+		TCP:       tcp,
+		Payload:   []byte{1, 2, 3, 4, 5, 6, 7},
+	}
+
+	coalesce.Start()
+	coalesce.insert(p)
+
+	if coalesce.pager.Used() != 1 {
+		t.Errorf("coalesce.pager.Used() not equal to 1\n")
+		t.Fail()
+	}
+
+	coalesce.Stop()
+}
