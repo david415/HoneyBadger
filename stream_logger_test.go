83b1b6e308fba6e460d37f634b9e2141c42f3a4c
David Stainton <dstainton415@gmail.com>
2015-02-15 10:03:10 +0000

    Add stream logger unit tests

---
+++ stream_logger_test.go
@@ -0,0 +1,65 @@
+package HoneyBadger
+
+import (
+	"bytes"
+	"code.google.com/p/gopacket"
+	"code.google.com/p/gopacket/layers"
+	"net"
+	"testing"
+)
+
+type TestSignalWriter struct {
+	lastWrite  []byte
+	signalChan chan bool
+	closeChan  chan bool
+}
+
+func NewTestSignalWriter() *TestSignalWriter {
+	return &TestSignalWriter{
+		signalChan: make(chan bool),
+		closeChan:  make(chan bool),
+	}
+}
+
+func (w *TestSignalWriter) Write(data []byte) (int, error) {
+	w.lastWrite = data
+	w.signalChan <- true
+	return len(data), nil
+}
+
+func (w *TestSignalWriter) Close() error {
+	w.closeChan <- true
+	return nil
+}
+
+func TestStreamLogger(t *testing.T) {
+	ipFlow, _ := gopacket.FlowFromEndpoints(layers.NewIPEndpoint(net.IPv4(1, 2, 3, 4)), layers.NewIPEndpoint(net.IPv4(2, 3, 4, 5)))
+	tcpFlow, _ := gopacket.FlowFromEndpoints(layers.NewTCPPortEndpoint(layers.TCPPort(1)), layers.NewTCPPortEndpoint(layers.TCPPort(2)))
+	flow := NewTcpIpFlowFromFlows(ipFlow, tcpFlow)
+
+	testWriter := NewTestSignalWriter()
+
+	streamLogger := NewStreamLogger("meow", flow)
+	streamLogger.writer = testWriter
+	streamLogger.Start()
+
+	want := []byte{1, 2, 3, 4, 5, 6, 7}
+	res := []Reassembly{
+		Reassembly{
+			Bytes: want,
+		},
+	}
+
+	go streamLogger.Reassembled(res)
+	<-testWriter.signalChan
+
+	if !bytes.Equal(testWriter.lastWrite, want) {
+		t.Errorf("stream log entry is wrong")
+		t.Fail()
+	}
+
+	go streamLogger.ReassemblyComplete()
+	<-testWriter.closeChan
+
+	go streamLogger.Stop()
+}
