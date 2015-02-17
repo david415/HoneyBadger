794d6ebd41f5ab66956384a86e9d59b9b4453c2a
David Stainton <dstainton415@gmail.com>
2015-02-17 00:33:45 +0000

    Teach service.go to pipeline packet processing
    Here we use goroutines and channels to construct the pipeline.
    This makes the code more readable, more performance efficient
    and easier to write unit tests for.

--- service.go
+++ service.go
@@ -31,6 +31,11 @@ import (
 
 const timeout time.Duration = time.Minute * 5 // XXX timeout connections after 5 minutes
 
+type TimedRawPacket struct {
+	Timestamp time.Time
+	RawPacket []byte
+}
+
 // InquisitorOptions are user set parameters for specifying the
 // details of how to proceed with honey_bager's TCP connection monitoring.
 // More parameters should soon be added here!
@@ -51,11 +56,19 @@ type InquisitorOptions struct {
 // with incoming packets weather they be from a pcap file or directly off the wire.
 type Inquisitor struct {
 	InquisitorOptions
-	stopChan     chan bool
-	connPool     *ConnectionPool
-	handle       *pcap.Handle
-	pager        *Pager
-	AttackLogger AttackLogger
+
+	stopCaptureChan    chan bool
+	decodePacketChan   chan TimedRawPacket
+	stopDecodeChan     chan bool
+	dispatchPacketChan chan PacketManifest
+	stopDispatchChan   chan bool
+
+	stopChan            chan bool
+	closeConnectionChan chan CloseRequest
+	connPool            *ConnectionPool
+	handle              *pcap.Handle
+	pager               *Pager
+	AttackLogger        AttackLogger
 }
 
 // NewInquisitor creates a new Inquisitor struct
@@ -72,9 +85,15 @@ func NewInquisitor(iface string, wireDuration time.Duration, filter string, snap
 			PacketLog:             packetLog,
 			StreamLog:             streamLog,
 		},
-		connPool:     NewConnectionPool(),
-		stopChan:     make(chan bool),
+		stopCaptureChan:     make(chan bool),
+		decodePacketChan:    make(chan TimedRawPacket),
+		stopDecodeChan:      make(chan bool),
+		dispatchPacketChan:  make(chan PacketManifest),
+		stopDispatchChan:    make(chan bool),
+		closeConnectionChan: make(chan CloseRequest),
+
 		pager:        NewPager(),
+		connPool:     NewConnectionPool(),
 		AttackLogger: NewAttackJsonLogger(logDir),
 	}
 	return &i
@@ -85,14 +104,21 @@ func (i *Inquisitor) Start() {
 	if i.handle == nil {
 		i.setupHandle()
 	}
+
+	go i.capturePackets()  // stopCaptureChan
+	go i.decodePackets()   // stopDecodeChan decodePacketChan
+	go i.dispatchPackets() // stopDispatchChan dispatchPacketChan
+
 	i.pager.Start()
 	i.AttackLogger.Start()
-	go i.receivePackets()
 }
 
 // Stop... stops the TCP attack inquisition!
 func (i *Inquisitor) Stop() {
-	i.stopChan <- true
+	i.stopDispatchChan <- true
+	i.stopDecodeChan <- true
+	i.stopCaptureChan <- true
+
 	i.AttackLogger.Stop()
 	i.handle.Close()
 	i.pager.Stop()
@@ -115,38 +141,11 @@ func (i *Inquisitor) setupHandle() {
 	}
 }
 
-func (i *Inquisitor) receivePackets() {
-	var eth layers.Ethernet
-	var ip layers.IPv4
-	var tcp layers.TCP
-	var payload gopacket.Payload
-	var conn *Connection
-
-	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip, &tcp, &payload)
-	decoded := make([]gopacket.LayerType, 0, 4)
-
-	closeConnectionChan := make(chan CloseRequest)
-
-	// XXX
-	ticker := time.Tick(timeout)
-	var lastTimestamp time.Time
+func (i *Inquisitor) capturePackets() {
 	for {
 		select {
-		case <-i.stopChan:
-			i.connPool.CloseAllConnections()
+		case <-i.stopCaptureChan:
 			return
-		case <-ticker:
-			if !lastTimestamp.IsZero() {
-				log.Printf("lastTimestamp is %s\n", lastTimestamp)
-				lastTimestamp = lastTimestamp.Add(timeout)
-				closed := i.connPool.CloseOlderThan(lastTimestamp)
-				if closed != 0 {
-					log.Printf("timeout closed %d connections\n", closed)
-				}
-			}
-		case closeRequest := <-closeConnectionChan:
-			i.connPool.Delete(*closeRequest.Flow)
-			closeRequest.CloseReadyChan <- true
 		default:
 			rawPacket, captureInfo, err := i.handle.ReadPacketData()
 			if err == io.EOF {
@@ -157,48 +156,96 @@ func (i *Inquisitor) receivePackets() {
 			if err != nil {
 				continue
 			}
+			i.decodePacketChan <- TimedRawPacket{
+				Timestamp: captureInfo.Timestamp,
+				RawPacket: rawPacket,
+			}
+		}
+	}
+}
+
+func (i *Inquisitor) decodePackets() {
+	var eth layers.Ethernet
+	var ip layers.IPv4
+	var tcp layers.TCP
+	var payload gopacket.Payload
+
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip, &tcp, &payload)
+	decoded := make([]gopacket.LayerType, 0, 4)
+
+	for {
+		select {
+		case <-i.stopDecodeChan:
+			return
+		case timedRawPacket := <-i.decodePacketChan:
 			newPayload := new(gopacket.Payload)
 			payload = *newPayload
-			err = parser.DecodeLayers(rawPacket, &decoded)
+			err := parser.DecodeLayers(timedRawPacket.RawPacket, &decoded)
 			if err != nil {
 				continue
 			}
 			flow := NewTcpIpFlowFromFlows(ip.NetworkFlow(), tcp.TransportFlow())
 			packetManifest := PacketManifest{
-				Timestamp: captureInfo.Timestamp,
+				Timestamp: timedRawPacket.Timestamp,
 				Flow:      flow,
-				RawPacket: rawPacket,
+				RawPacket: timedRawPacket.RawPacket,
 				IP:        ip,
 				TCP:       tcp,
 				Payload:   payload,
 			}
-			if i.connPool.Has(flow) {
-				conn, err = i.connPool.Get(flow)
+			i.dispatchPacketChan <- packetManifest
+		}
+	}
+}
+
+func (i *Inquisitor) dispatchPackets() {
+	var conn *Connection
+	var err error
+
+	ticker := time.Tick(timeout)
+	var lastTimestamp time.Time
+	for {
+		select {
+		case <-ticker:
+			if !lastTimestamp.IsZero() {
+				log.Printf("lastTimestamp is %s\n", lastTimestamp)
+				lastTimestamp = lastTimestamp.Add(timeout)
+				closed := i.connPool.CloseOlderThan(lastTimestamp)
+				if closed != 0 {
+					log.Printf("timeout closed %d connections\n", closed)
+				}
+			}
+		case <-i.stopDispatchChan:
+			return
+		case closeRequest := <-i.closeConnectionChan:
+			i.connPool.Delete(*closeRequest.Flow)
+			closeRequest.CloseReadyChan <- true
+		case packetManifest := <-i.dispatchPacketChan:
+			if i.connPool.Has(packetManifest.Flow) {
+				conn, err = i.connPool.Get(packetManifest.Flow)
 				if err != nil {
 					panic(err) // wtf
 				}
 			} else {
 				// XXX TODO: start the loggers in the connection goroutine instead of this one?
-				conn = NewConnection(closeConnectionChan, i.pager, i.InquisitorOptions.BufferedPerConnection, i.InquisitorOptions.BufferedTotal)
+				conn = NewConnection(i.closeConnectionChan, i.pager, i.InquisitorOptions.BufferedPerConnection, i.InquisitorOptions.BufferedTotal)
 				conn.AttackLogger = i.AttackLogger
 				if i.PacketLog {
-					conn.PacketLogger = NewPcapLogger(i.LogDir, flow)
+					conn.PacketLogger = NewPcapLogger(i.LogDir, packetManifest.Flow)
 					conn.PacketLogger.Start()
 				}
 				if i.StreamLog {
-					clientStream := NewStreamLogger(i.LogDir, flow)
+					clientStream := NewStreamLogger(i.LogDir, packetManifest.Flow)
 					clientStream.Start()
 					conn.ClientStream = clientStream
-
-					serverStream := NewStreamLogger(i.LogDir, flow.Reverse())
+					serverStream := NewStreamLogger(i.LogDir, packetManifest.Flow.Reverse())
 					serverStream.Start()
 					conn.ServerStream = serverStream
 				}
-				i.connPool.Put(flow, conn)
+				i.connPool.Put(packetManifest.Flow, conn)
 				conn.Start(true)
 			}
 			conn.receivePacket(&packetManifest)
-			lastTimestamp = captureInfo.Timestamp
 		}
 	}
 }
